0e2d305b160ed5b2f33825420ce810ca91595f52
Obed <niobed.nexus@gmail.com>
2025-02-14 17:41:26 +0200

    updating UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and singing" />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   );
 }
