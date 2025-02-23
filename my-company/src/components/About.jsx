e8ebcd7d52809fdb889b8b13dabf3e739a508bf3
Obed <niobed.nexus@gmail.com>
2025-02-23 23:43:05 +0200

    add routing website

---
+++ my-company/src/components/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our company we have been providing the top notch service.
+      </p>
+    </div>
+  );
+}
+
+export default About;
