f0cc163c767398e63cd5497fd5aca66e67f50f30
Obed <niobed.nexus@gmail.com>
2025-03-07 01:12:53 +0200

    update FormikForm

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -8,6 +8,14 @@ const initialValues = {
   password: "",
 };
 
+const RegistrationSchema = Yup.object().shape({
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
+
 function FormikForm() {
   const { values, handleBlur, handleChange, handleSubmit } = useFormik({
     initialValues,
