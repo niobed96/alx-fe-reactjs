1c81bfe4e58987a7bc017e9d313798e7366ec357
Obed <niobed.nexus@gmail.com>
2025-03-07 12:24:32 +0200

    update FormikForm file

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
-import RegistrationSchema from "../schemas";
+import * as yup from "yup";
+
+const passwordRegex = new RegExp(
+  /^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$/
+);
 
 const initialValues = {
   username: "",
   email: "",
   password: "",
 };
+
+const RegistrationSchema = yup.object({
+  username: yup.string().min(4).required("Please Enter USername"),
+  email: yup
+    .string()
+    .email("Please Enter VAlid Email")
+    .required("Please Enter Your Email"),
+  password: yup
+    .string()
+    .matches(passwordRegex, "Please Enter Valid Password")
+    .required("Please Enter Your password"),
+});
 
 function formikForm() {
   const onSubmit = (values, actions) => {
