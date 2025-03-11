c38a1356d18e35ae6e982849dc460412cb440005
Obed <niobed.nexus@gmail.com>
2025-03-11 18:29:24 +0200

    Adding UserProfile component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello Tailwind</h1>
+        <UserProfile />
       </div>
     </>
   );
