d781f2ee9da3d8447e89d83899d070a7a261e9f6
Obed <niobed.nexus@gmail.com>
2025-03-11 18:50:12 +0200

    Adding animation to UserProfile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full sm:w-24 sm:h-24 md:w-36 md: md:h-36 mx-auto hover:scale-100 transition-transform duration-300 ease-out"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md: md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">
         John Doe
